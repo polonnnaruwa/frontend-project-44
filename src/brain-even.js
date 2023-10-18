393591e883153c361c89d9f6f4b6ab946259287a
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2023-10-18 10:32:39 +0400

    all

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,4 +1,6 @@
 import readlineSync from 'readline-sync';
+import message from './cli.js';
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -8,67 +10,32 @@ function getRandomInt(min, max) {
 const isEven = (number) => number % 2 === 0;
 
 const brainEvenGame = () => {
+    const name = message();
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
     
-    let random = getRandomInt(1, 20);
-    console.log(`'Question:'${random}`);
+    for (let i = 0; i < 3; i += 1) {
 
-    let answer = readlineSync.question('Your answer:');
-         
-    if (isEven(random) && answer === 'yes' ) {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'no') {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'yes') {
-        console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, Bill!');
-        return; 
-    }
-    else if (isEven(random) && answer === 'no') {
-        console.log('"no" is wrong answer ;(. Correct answer was "yes". Let\'s try again, Bill!');
-        return;
+        let random = getRandomInt(1, 20);
+        console.log(`'Question:'${random}`);
+
+        let answer = readlineSync.question('Your answer:');
+        
+        if (isEven(random) && answer === 'yes' ) {
+            console.log('Correct!');
+        }
+        else if (!isEven(random) && answer === 'no') {
+            console.log('Correct!');
+        }
+        else if (!isEven(random) && answer === 'yes') {
+            console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, Bill!');
+            return; 
+        }
+        else if (isEven(random) && answer === 'no') {
+            console.log('"no" is wrong answer ;(. Correct answer was "yes". Let\'s try again, Bill!');
+            return;
+        }
     }
  
-    random = getRandomInt(1, 20);
-    console.log(`'Question:'${random}`);
-
-    answer = readlineSync.question('Your answer:');
-         
-    if (isEven(random) && answer === 'yes' ) {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'no') {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'yes') {
-        console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, Bill!');
-        return; 
-    }
-    else if (isEven(random) && answer === 'no') {
-        console.log('"no" is wrong answer ;(. Correct answer was "yes". Let\'s try again, Bill!');
-        return;
-    }
-
-    random = getRandomInt(1, 20);
-    console.log(`'Question:'${random}`);
-
-    answer = readlineSync.question('Your answer:');
-         
-    if (isEven(random) && answer === 'yes' ) {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'no') {
-        console.log('Correct!');
-    }
-    else if (!isEven(random) && answer === 'yes') {
-        console.log('"yes" is wrong answer ;(. Correct answer was "no". Let\'s try again, Bill!');
-        return; 
-    }
-    else if (isEven(random) && answer === 'no') {
-        console.log('"no" is wrong answer ;(. Correct answer was "yes". Let\'s try again, Bill!');
-        return;
-    }
-    console.log('Congratulations, Bill!');
+    console.log(`Congratulations, ${name}!`);
 };
 export default brainEvenGame;
--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,8 @@ import readlineSync from 'readline-sync';
 const message = () => {
     console.log("Welcome to the brain Games!")   
     const name = readlineSync.question('May I have your name?');
-    console.log(`"Hello",${name}!`);    
+    console.log(`Hello, ${name}!`);
+    return name;   
 };
 export default message;
 
