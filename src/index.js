ea8b018680fc8bc5ed4fbc42c762eb227c02a8cd
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2023-10-19 14:34:08 +0400

    brain-calc

--- src/index.js
+++ src/index.js
@@ -4,65 +4,74 @@ import message from './cli.js';
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.round(Math.random() * (max - min)) + min;
 }
 
 const brainCalc = () => {
     const name = message();
     console.log('What is the result of the expression?');
 
-    for (let i = 0; i < 1; i += 1) {
-        let A = getRandomInt(1, 20);
-        let B = getRandomInt(1, 20);
-        const sum = A + B;
+    for (let i = 0; i < 3; i += 1) {
+        let A, B, answer;
+        const operation = getRandomInt(0, 2);
+        switch (operation) {
+        case 0: 
+            A = getRandomInt(1, 20);
+            B = getRandomInt(1, 20);
+            const sum = A + B;
 
-        console.log(`Question: ${A} + ${B}`);
+            console.log(`Question: ${A} + ${B}`);
 
-        let answer = readlineSync.question('Your answer: ');
+            answer = readlineSync.question('Your answer: ');
 
-        if (parseInt(answer) === sum ) {
-            console.log('Correct!');
-        } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${sum}. Let's try again, ${name}!`);
-            return;
-        }
-    console.log(`Congratulations, ${name}!`);
-    }
+            if (parseInt(answer) === sum ) {
+                console.log('Correct!');
+            } else {
+                console.log(`${answer} is wrong answer ;(. Correct answer was ${sum}. Let's try again, ${name}!`);
+                return;
+            }
+            console.log(`Congratulations, ${name}!`);
+            break;
+
+        case 1: 
+            A = getRandomInt(1, 20);
+            B = getRandomInt(1, 20);
+            const subtraction = A - B;
+
+            console.log(`Question: ${A} - ${B}`);
+
+            answer = readlineSync.question('Your answer: ');
+
+            if (parseInt(answer) === subtraction) {
+                console.log('Correct!');
+            } else {
+                console.log(`${answer} is wrong answer ;(. Correct answer was ${subtraction}. Let's try again, ${name}!`);
+                return;
+            }
+            console.log(`Congratulations, ${name}!`);
+            break;
+
+        case 2:
+            A = getRandomInt(1, 20);
+            B = getRandomInt(1, 20);
+            const multiplication = A * B;
     
-    for (let i = 0; i < 1; i += 1) {
-        let A = getRandomInt(1, 20);
-        let B = getRandomInt(1, 20);
-        const subtraction = A - B;
-
-        console.log(`Question: ${A} - ${B}`);
-
-        let answer = readlineSync.question('Your answer: ');
-
-        if (parseInt(answer) === subtraction) {
-            console.log('Correct!');
-        } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${subtraction}. Let's try again, ${name}!`);
-            return;
-        }
-    console.log(`Congratulations, ${name}!`);
-    }
+            console.log(`Question: ${A} * ${B}`);
     
-    for (let i = 0; i < 1; i += 1) {
-        let A = getRandomInt(1, 20);
-        let B = getRandomInt(1, 20);
-        const multiplication = A * B;
-
-        console.log(`Question: ${A} * ${B}`);
-
-        let answer = readlineSync.question('Your answer: ');
-
-        if (parseInt(answer) === multiplication) {
-            console.log('Correct!');
-        } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${multiplication}. Let's try again, ${name}!`);
-            return;
+            answer = readlineSync.question('Your answer: ');
+    
+            if (parseInt(answer) === multiplication) {
+                console.log('Correct!');
+            } else {
+                console.log(`${answer} is wrong answer ;(. Correct answer was ${multiplication}. Let's try again, ${name}!`);
+                return;
+            }
+            console.log(`Congratulations, ${name}!`);
+            break;
+        default:
+            console.log("default");
         }
-    console.log(`Congratulations, ${name}!`);
+       
     }
 };
 
