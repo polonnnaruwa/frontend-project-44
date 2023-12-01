8e485cb4b76ec299c5693bd5ca7b0cdea451dd4b
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2023-12-01 12:19:48 +0400

    bin brain

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const message = () => {
-  console.log('Welcome to the brain Games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
