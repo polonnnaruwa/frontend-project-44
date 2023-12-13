150b54d6a4d366902984d7551e5fb58a165dff21
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2023-12-13 11:21:37 +0400

    progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,9 +3,9 @@ import runEngine from '../index.js';
 
 const maxArrayLength = 9;
 
-const makeProgression = (step, startNumber) => {
+const makeProgression = (step, startNumber, progressionLength) => {
   const array = [];
-  for (let i = 0; i <= maxArrayLength; i += 1) {
+  for (let i = 0; i <= progressionLength; i += 1) {
     const newNumber = startNumber + step * i;
     array.push(newNumber);
   }
@@ -16,12 +16,12 @@ export const rule = 'What number is missing in the progression?';
 export const runBrainProgression = () => {
   const step = getRandomNumber(1, 4);
   const number = getRandomNumber(1, 4);
-  const array = makeProgression(step, number);
+  const progression = makeProgression(step, number, maxArrayLength);
   const index = getRandomNumber(0, maxArrayLength);
 
-  const result = array[index];
-  array[index] = '..';
-  const strArray = array.join(' ');
+  const result = progression[index];
+  progression[index] = '..';
+  const strArray = progression.join(' ');
 
   const question = `Question: ${strArray}`;
   return [question, result.toString()];
