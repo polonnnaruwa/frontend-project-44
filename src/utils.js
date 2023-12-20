3622d3d415b2b72d839df3635f069df64e423a66
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2023-12-20 22:24:10 +0400

    utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
 export default function getRandomNumber(min, max) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return Math.round(Math.random() * (newMax - newMin)) + newMin;
+  return Math.round(Math.random() * (max - min)) + min;
 }
